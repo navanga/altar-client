41ad72739a555cfada7b5601391f08c2950ed153
navanga <navanga@gmail.com>
2018-03-30 21:59:30 +1100

    First commit

---
+++ src/main/java/com/navanga/altar/altarclient/AltarClientApplication.java
@@ -0,0 +1,12 @@
+package com.navanga.altar.altarclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltarClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltarClientApplication.class, args);
+	}
+}
---
+++ src/test/java/com/navanga/altar/altarclient/AltarClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.navanga.altar.altarclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AltarClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
